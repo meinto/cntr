b19ceacf4a1c32125a41db5b587c876618229919
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2020-02-28 20:22:14 +0100

    Fix Info.plist

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf ./dist
 mkdir -p dist/cntr.app/Contents/MacOS/
 mkdir -p dist/cntr.app/Contents/Resources/
 
-cp build-assets/macos/Info.plist dist/cntr.app/
+cp build-assets/macos/Info.plist dist/cntr.app/Contents/
 cp build-assets/macos/cntr.icns dist/cntr.app/Contents/Resources/
 
 go build -o dist/cntr.app/Contents/MacOS/cntr .
