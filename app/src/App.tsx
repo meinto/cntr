7767ad9e2721e6b194f23bca6ac4ea598c191e3a
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2020-03-01 22:38:27 +0100

    Styleings

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { LineChart, XAxis, YAxis, CartesianGrid, Line, Tooltip } from 'recharts';
+import { ResponsiveContainer, XAxis, YAxis, CartesianGrid, Area, Tooltip, AreaChart } from 'recharts';
 import useInterval from '@use-it/interval';
 import { useDataFetch } from './useDataFetch'
 
@@ -19,14 +19,26 @@ function App() {
       <h1 style={{marginBottom: 0}}>Statistics</h1>
       <strong>(last 10 days)</strong>
       <br />
-      <LineChart width={500} height={300} data={chartData}>
-        <XAxis dataKey="name"/>
-        <YAxis/>
-        <Tooltip />
-        <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
-        <Line type="monotone" dataKey="keys" stroke="#8884d8" />
-        <Line type="monotone" dataKey="clicks" stroke="#82ca9d" />
-      </LineChart>
+      <ResponsiveContainer aspect={2.5}>
+        <AreaChart data={chartData}>
+          <defs>
+            <linearGradient id="colorKeys" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+            </linearGradient>
+            <linearGradient id="colorClicks" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name"/>
+          <YAxis/>
+          <Tooltip contentStyle={{color: '#333'}}/>
+          <CartesianGrid stroke="#aaa" strokeDasharray="3 3"/>
+          <Area type="monotone" dataKey="keys" stroke="#8884d8" fill="url(#colorKeys)" />
+          <Area type="monotone" dataKey="clicks" stroke="#82ca9d" fill="url(#colorClicks)" />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   );
 }
